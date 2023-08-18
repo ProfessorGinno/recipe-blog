f5aba5aec4612ea43c5955f1847b10754b71fed5
Maremoto <mmontesto616@gmail.com>
2023-08-17 21:13:56 -0500

    feat : blogs models, views, and urls created

--- recipes_blog/urls.py
+++ recipes_blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, CreatePost
+
 
 urlpatterns = [
     path('blog-index/', index, name='blog_index'),
+    path('create-post/',CreatePost.as_view(), name='create_post'),
 ]
